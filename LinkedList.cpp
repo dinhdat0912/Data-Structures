ed2aa7fb92ef7b34c508a6582e05c059103a38f6
Thanh Dat Dinh <dinhdat0912@gmail.com>
2020-12-06 13:45:43 -0800

    Initial commit

---
+++ LinkedList.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+class Node{	
+	public:
+	int data;
+	Node *next;
+	Node(int data){
+		this->data=data;
+		next=NULL;
+	}
+};
+void print(Node *head){
+	while(head!=NULL){
+		cout<<head->data;
+		head=head->next;
+	}
+}
+int main(){
+	Node n1(1);
+	Node *head = &n1;
+	Node n2(2);
+	Node n3(3);
+	Node n4(4);
+	Node n5(5);
+	
+	n1.next = &n2;
+	n2.next = &n3;
+	n3.next = &n4;
+	n4.next = &n5;
+	print(head);
+//	cout<<n1.data<<" "<<n2.data<<endl;
+	
+//	cout<<head->data;
+//	Node *n3 = new Node(3);
+//	Node *n4 = new Node(4);
+//	n3->next = n4;
+//	return 0;
+}
